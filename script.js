93384c3258bbe9548b14c48fd3c95b922e3ae724
Afiffudin-au <afiffudin077@gmail.com>
2020-06-08 19:31:38 +0700

    Add All Files

---
+++ script.js
@@ -0,0 +1,102 @@
+const ButtonGnrt =  document.querySelector('.generate');
+const content = document.querySelector('.content-name')
+const selectRegion = document.querySelector('.select-region');
+const selectSort = document.querySelector('.SelectSort');
+const option = document.querySelector('.select');
+let arr = [];
+let tempArr = [];
+function readTextFile(file)
+{
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function ()
+  {       
+    arr.push( rawFile.responseText.split(","));
+  }
+rawFile.send(null);
+}
+
+
+function handleSelectSort(){
+  let string = '';
+  if(this.value == 'sortNameDown'){
+    if(tempArr[0] != undefined){    
+      const filters =  tempArr.sort((a,b)=> a > b ? 1 : -1);
+      content.innerHTML = filters.map((filter,i)=>`${i+1}.${filter} <br>`).join('');
+    }
+  }
+  if(this.value == 'sortNameUp'){
+    if(tempArr[0] != undefined){
+      const filters =  tempArr.sort((a,b)=> a < b ? 1 : -1);
+      content.innerHTML = filters.map((filter,i)=>`${i+1}.${filter} <br>`).join('');
+    }
+  }
+}
+function filterRegion(e)
+{
+  if(this.value == 'Indonesia'){
+    arr = [];
+    readTextFile("Indonesian-name.txt"); 
+  }
+  if(this.value == 'Amerika'){
+    arr = [];
+    readTextFile("American-name.txt"); 
+  }
+}
+
+function Generate_Name(){
+  //clear array tidak ada penumpukan dalam temp array
+  //jika tombol diklik akan mengisi tempArr tersebut
+  //jika tombol dikik lagi itu akan menghapus isi tempArr dan di isi yang baru
+  //jika tidak diatasi saat menampilkan tempArr , output tidak sesuai dengar harapan
+  tempArr = []; 
+  let number = document.querySelector('.input-loop').value;
+  let string = '';
+  if(number == ''){
+    alert('please fill input box');
+    return;
+  }
+  if(number>1000){
+    content.innerText = 'Please Input Under 1000';
+    return;
+  }else{
+    for(let i = 1; i<=number;i++){ 
+      const randomNumber = Math.round(Math.random()* arr[0].length);
+      
+      if(arr[0][randomNumber-i]!=undefined){
+        string += `${i} .${arr[0][randomNumber]} <br>`
+        //fill array
+        tempArr.push(Array.from(arr[0][randomNumber-i].split(",")));
+      } 
+      if(arr[0][randomNumber-i]==undefined){
+        const randomNumber1 = Math.round(Math.random()* arr[0].length);    
+        if(arr[0][randomNumber1]!=undefined){ 
+          string += `${i} .${arr[0][randomNumber1]} <br>`
+          //fill array
+          tempArr.push(Array.from(arr[0][randomNumber1].split(",")));  
+        }
+        if(arr[0][randomNumber1]==undefined){
+          const randomNumber2 = Math.round(Math.random()* arr[0].length);
+          if(arr[0][randomNumber2]!=undefined){
+            string += `${i} .${arr[0][randomNumber2]} <br>`
+            //fill array
+            tempArr.push(Array.from(arr[0][randomNumber2].split(",")));
+          }            
+        }
+      }      
+   }
+  }
+ content.innerHTML = string; 
+}
+selectRegion.addEventListener('change',filterRegion);
+ButtonGnrt.addEventListener('click',Generate_Name);
+selectSort.addEventListener('change',handleSelectSort);
+
+    // for(let j = 0; j<arr.length; j++){
+    
+    //   for(let i = 0; i<arr[j].length; i++){
+    //   console.log(arr[j].length);   
+    //   }
+   
+    // }
+  
